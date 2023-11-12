889fdc9c073745535b7d9a1a30ea24e7d5046505
ashleymerritt <ashley.merritt@uconn.edu>
2023-11-12 16:46:05 -0500

    Changing of code in order to leave out "ID" and "Outcome" from table 1

--- code/Descriptivestatistics.R
+++ code/Descriptivestatistics.R
@@ -1,8 +1,12 @@
+exclude_vars <- c("Id", "Outcome")
+
+subset_data <- diabetes[, !(names(diabetes) %in% exclude_vars)]
+
 desc_stats <- data.frame(
-  Variable = colnames(diabetes),
-  Mean = colMeans(diabetes, na.rm = TRUE),
-  SD = apply(diabetes, 2, sd, na.rm = TRUE),
-  Median = apply(diabetes, 2, median, na.rm = TRUE)
+  Variable = colnames(subset_data),
+  Mean = colMeans(subset_data, na.rm = TRUE),
+  SD = apply(subset_data, 2, sd, na.rm = TRUE),
+  Median = apply(subset_data, 2, median, na.rm = TRUE)
 )
 
 latex_table_xtable <- xtable(desc_stats, caption = "Descriptive Statistics")
