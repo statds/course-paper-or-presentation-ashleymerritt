f58f5aff16955502d64481983ee0db6e81ec9670
ashleymerritt <ashley.merritt@uconn.edu>
2023-12-05 22:19:58 -0500

    Decision tree structure code change

--- code/Decision Tree Structure Code.R
+++ code/Decision Tree Structure Code.R
@@ -3,11 +3,14 @@ library(rpart)
 library(rpart.plot)
 
 set.seed(30)
-train_indices <- sample(seq_len(nrow(diabetes)), size = 0.8 * nrow(diabetes))
+train_indices <- sample(1:(nrow(diabetes)), 0.8 * nrow(diabetes))
 train_data <- diabetes[train_indices, ]
 test_data <- diabetes[-train_indices, ]
 
-tree_model <- rpart(Outcome ~ Pregnancies + Glucose + BloodPressure + SkinThickness 
-             + Insulin + BMI + DiabetesPedigreeFunction + Age, 
-             data = train_data, method = "class")
+tree_model <- rpart(Outcome ~ ., data = train_data, method = "class", control = rpart.control(cp = 0.01))
 rpart.plot(tree_model, box.palette = "auto", main = "Decision Tree Structure")
+
+pdf("decision_tree_plot.pdf")
+rpart.plot(tree_model, box.palette = "auto", main = "Decision Tree Structure")
+dev.off()
+
