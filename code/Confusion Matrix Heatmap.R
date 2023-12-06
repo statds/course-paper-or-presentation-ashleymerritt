8093c049745e3becce493ddc12d351cb37fee2eb
ashleymerritt <ashley.merritt@uconn.edu>
2023-12-05 23:22:04 -0500

    Confusion matrix heatmap

---
+++ code/Confusion Matrix Heatmap.R
@@ -0,0 +1,14 @@
+predictions <- predict(tree_model, newdata = test_data, type = "class")
+
+test_data$Outcome <- factor(test_data$Outcome, levels = levels(predictions))
+
+conf_matrix <- table(Actual = test_data$Outcome, Predicted = predictions)
+
+library(ggplot2)
+ggplot(as.data.frame.table(conf_matrix), aes(x = Actual, y = Predicted, fill = Freq)) +
+  geom_tile() +
+  scale_fill_gradient(low = "white", high = "blue") +
+  geom_text(aes(label = Freq), vjust = 1) +
+  labs(title = "Confusion Matrix Heatmap", x = "Actual Outcome", y = "Predicted Outcome")
+
+
